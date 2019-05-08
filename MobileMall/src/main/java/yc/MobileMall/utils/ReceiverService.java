dd0a952d7d17d75f8bf6a27716435cbece101207
Administrator <Administrator@FL5-xin>
2019-05-08 21:26:09 +0800

    收件人关联user

--- MobileMall/src/main/java/yc/MobileMall/utils/ReceiverService.java
+++ MobileMall/src/main/java/yc/MobileMall/utils/ReceiverService.java
@@ -1,9 +1,13 @@
 package yc.MobileMall.utils;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import yc.MobileMall.bean.Receiver;
+import yc.MobileMall.bean.ReceiverExample;
+import yc.MobileMall.bean.ReceiverExample.Criteria;
 import yc.MobileMall.dao.ReceiverMapper;
 
 @Service
@@ -17,11 +21,22 @@ public class ReceiverService {
 	 * @throws BizException 
 	 */
 	public void saveReceiver(Receiver rec) throws BizException {
-		// receiverMapper.insert(rec);  字段全部添加，没有的设置为null
-		int i=0;
-		i=receiverMapper.insertSelective(rec);  //主键是自动添加的，默认插入为空
-		if(i<1){
-			throw new BizException("保存失败");
+		//先查询是否已经存在
+		ReceiverExample example=new ReceiverExample();
+		Criteria cri=example.createCriteria();
+		cri.andFirstNameEqualTo(rec.getFirstName()).andLastNameEqualTo(rec.getLastName()).andCountryEqualTo(rec.getCountry());
+		cri.andProvinceEqualTo(rec.getProvince()).andCityEqualTo(rec.getCity()).andAddressEqualTo(rec.getAddress());
+		cri.andEmailEqualTo(rec.getEmail()).andTelephoneEqualTo(rec.getTelephone()).andUserIdEqualTo(rec.getUserId());
+		List<Receiver> ulist=receiverMapper.selectByExample(example);
+		
+		if(ulist.size()>0){                        //收件信息已存在则不插入数据库
+			
+		}else{
+			int i=0;
+			i=receiverMapper.insertSelective(rec);  //主键是自动添加的，默认插入为空
+			if(i<1){
+				throw new BizException("保存失败");
+			}
 		}
 	}
 	
